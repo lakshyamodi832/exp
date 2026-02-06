55b34232bd2a993d055d7321fd9619615c140068
lakshyamodi832 <lakshyaymodi@gmail.com>
2026-02-06 22:55:42 +0530

    five

---
+++ src/com/exp/four/sBuffer.java
@@ -0,0 +1,9 @@
+package com.exp.four;
+
+public class sBuffer {
+    static void main() {
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.append(" World!");
+        System.out.println(sb);
+    }
+}
---
+++ src/com/exp/four/string.java
@@ -0,0 +1,14 @@
+package com.exp.four;
+
+public class string {
+    static void main() {
+        String s1 = "Hello ";
+        String s2 = "World!";
+
+        String s3 = s1 + s2;
+        String s4 = s1.concat(s2);
+
+        System.out.println(s3);
+        System.out.println(s4);
+    }
+}
